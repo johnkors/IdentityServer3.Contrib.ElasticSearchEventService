ebfd51b6bb271f35ee08912ac5ae67a8f6153271
John Korsnes <johnkors@gmail.com>
2015-01-27 05:05:03 +0100

    paths in ps1

--- updatenuspecversion.ps1
+++ updatenuspecversion.ps1
@@ -1,7 +1,9 @@
-$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$root = $env:APPVEYOR_BUILD_FOLDER
 $version = $env:APPVEYOR_BUILD_VERSION
+$nuspecPath = $root\source\Contrib.sln\Thinktecture.IdentityServer.Services.Contrib\ThinkTecture.IdentityServer.Services.Contrib.nuspec
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
+Write-Host $nuspecPath
 Write-Host "Setting .nuspec version tag to $versionStr"
 
 $content = (Get-Content $root\source\Contrib.sln\Thinktecture.IdentityServer.Services.Contrib\ThinkTecture.IdentityServer.Services.Contrib.nuspec) 
